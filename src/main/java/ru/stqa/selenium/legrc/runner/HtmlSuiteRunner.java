23a63a52bb6cdcc748b63d9d1ea958f6a4d9a408
Alexei Barantsev <barancev@gmail.com>
2016-01-05 23:49:51 +0300

    Parsing the suite/scenario file

--- src/main/java/ru/stqa/selenium/legrc/runner/HtmlSuiteRunner.java
+++ src/main/java/ru/stqa/selenium/legrc/runner/HtmlSuiteRunner.java
@@ -2,6 +2,15 @@ package ru.stqa.selenium.legrc.runner;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import org.cyberneko.html.parsers.DOMParser;
+import org.w3c.dom.Document;
+import org.w3c.dom.traversal.DocumentTraversal;
+import org.w3c.dom.traversal.NodeFilter;
+import org.w3c.dom.traversal.TreeWalker;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
 
 public class HtmlSuiteRunner {
 
@@ -17,14 +26,22 @@ public class HtmlSuiteRunner {
   @Parameter(names = {"--report", "-r"}, description = "Report file")
   private String report;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException, SAXException {
     HtmlSuiteRunner runner = new HtmlSuiteRunner();
     new JCommander(runner, args);
     runner.run();
   }
 
-  private void run() {
+  private void run() throws IOException, SAXException {
+    File toRun = new File(suiteOrScenario);
 
+    DOMParser parser = new DOMParser();
+    parser.parse(toRun.getAbsolutePath());
+    Document doc = parser.getDocument();
+
+    DocumentTraversal traversal = (DocumentTraversal) doc;
+    TreeWalker walker = traversal.createTreeWalker(doc.getDocumentElement(), NodeFilter.SHOW_ALL, null, false);
+    
   }
 
 }
